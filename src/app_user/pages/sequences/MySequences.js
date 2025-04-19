f1b53540f0c2f60f4dafaeb3f645f8e29d3498b1
marcellypereira <marcellyavelino020@gmail.com>
2025-04-19 14:26:00 -0300

    atualizacoes

--- src/app_user/pages/sequences/MySequences.js
+++ src/app_user/pages/sequences/MySequences.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import BaseLayout from '../../../app/BaseLayout';
 import Accordion from '../../../app/components/Accordion';
