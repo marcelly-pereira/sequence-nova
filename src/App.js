c36f3bdb9e15cdabe1877594b205ac616e4b8d78
marcellypereira <marcellyavelino020@gmail.com>
2025-04-21 14:18:20 -0300

    atualizações

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
 import BaseLayout from './app/BaseLayout';
 import Login from './app_user/Login';
 import './App.css';
@@ -12,78 +17,49 @@ import Companies from './app_user/pages/companies/ActiveClients';
 import Automations from './app_user/pages/personalization/Automations/Automations';
 import Registrations from './app_user/pages/personalization/Registrations';
 import User from './app_user/User';
-import Notification from './app_user/Notification';
+import Notification from './app_user/notification/Notification';
 import Record from './app_user/pages/companies/Record/Record';
 import DeploymentCard from './app_user/pages/personalization/Automations/DeploymentCard';
 import DraggableCard from '../src/app_user/DraggableCard';
 
 function App() {
-        return (
-                <Router>
-                        <Routes>
-                                <Route path="/" element={<Login />} />
-                                <Route path="/login" element={<Login />} />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
 
-                                <Route path="/home" element={
-                                        <Home />
-                                } />
+        <Route path="/home" element={<Home />} />
 
-                                <Route path="/baseLayout" element={
-                                        <BaseLayout />
-                                }>
-                                </Route>
+        <Route path="/baseLayout" element={<BaseLayout />}></Route>
 
-                                <Route path="/sequencia" element={
-                                        <MySequences />
-                                } />
+        <Route path="/sequencia" element={<MySequences />} />
 
-                                <Route path="/templates" element={
-                                        <TemplatesSequencia />
-                                } />
+        <Route path="/templates" element={<TemplatesSequencia />} />
 
-                                <Route path="/tarefas" element={
-                                        <Lists />
-                                } />
+        <Route path="/tarefas" element={<Lists />} />
 
-                                <Route path="/detalhes_tarefas" element={
-                                        <Recurrent />
-                                } />
+        <Route path="/detalhes_tarefas" element={<Recurrent />} />
 
-                                <Route path="/empresas" element={
-                                        <Companies />
-                                } />
+        <Route path="/empresas" element={<Companies />} />
 
-                                <Route path="/configurações" element={
-                                        <Registrations />
-                                } />
+        <Route path="/configurações" element={<Registrations />} />
 
-                                <Route path="/lista-automacoes" element={
-                                        <Automations />
-                                } />
+        <Route path="/lista-automacoes" element={<Automations />} />
 
-                                <Route path="/user" element={
-                                        <User />
-                                } />
+        <Route path="/user" element={<User />} />
 
-                                <Route path="/notification" element={
-                                        <Notification />
-                                } />
+        <Route path="/notification" element={<Notification />} />
 
-                                <Route path="/record" element={
-                                        <Record />
-                                } />
+        <Route path="/record" element={<Record />} />
 
-                                <Route path="/deploymentcard" element={
-                                        <DeploymentCard />
-                                } />
-                                <Route path="/draggable-card" element={
-                                        <DraggableCard />
-                                } />
+        <Route path="/deploymentcard" element={<DeploymentCard />} />
+        <Route path="/draggable-card" element={<DraggableCard />} />
 
-                                <Route path="*" element={<Navigate to="/" replace />} />
-                        </Routes>
-                </Router>
-        );
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
